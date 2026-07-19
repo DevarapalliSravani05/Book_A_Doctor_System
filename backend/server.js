4a9d52b22e35eb4aacd568757481c421a99f246b
Sravani <sravanid987@gmail.com>
2026-07-19 10:38:27 +0530

    Temporarily allow all origins

--- backend/server.js
+++ backend/server.js
@@ -15,19 +15,14 @@ connectDB();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://book-a-doctor-system-e33j-r9ratemfr-sravs5.vercel.app"
+];
+
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (
-        !origin ||
-        origin === "http://localhost:5173" ||
-        origin.endsWith(".vercel.app")
-      ) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
   })
 );
